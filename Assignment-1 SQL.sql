36795a44b72ad435d683931a0f2b2c25b9b01e0d
Murat Haris Bulga <muratbulga@gmail.com>
2023-01-13 20:59:15 +0300

    Update Assignment-1 SQL.sql

--- Assignment-1 SQL.sql
+++ Assignment-1 SQL.sql
@@ -10,36 +10,40 @@ SELECT  [customer_id]
       ,[zip_code]
 FROM [SampleRetail].[sale].[customer]
 
-
-SELECT DISTINCT city,
-MAX(customer_id) AS customer_city
+SELECT  city,
+COUNT(customer_id) AS customers_city
 FROM sale.customer 
 GROUP BY city
-ORDER BY  customer_city  DESC;
+ORDER BY  customers_city  DESC;
 
-SELECT  product_id,
-SUM(order_id) AS total_quantity
+
+SELECT order_id, SUM(quantity) AS total_quantity
 FROM sale.order_item 
-GROUP BY product_id
-ORDER BY total_quantity;
+GROUP BY order_id
 
 
-SELECT order_date, customer_id FROM sale.orders;
 
-SELECT order_id,
-SUM(quantity) AS total_amount
+SELECT customer_id, MIN(order_date) AS order_date
+FROM sale.orders
+GROUP BY customer_id;
+
+
+SELECT  order_id, SUM(quantity*list_price* (1- discount)) total_amount
 FROM sale.order_item
 GROUP BY order_id
-ORDER BY total_amount DESC;
 
 
-SELECT MAX(list_price) AS max_price
-FROM sale.order_item;
+
+SELECT TOP 1 order_id, AVG(list_price) avg_product_price
+FROM sale.order_item
+GROUP BY order_id
+ORDER BY avg_product_price  DESC;
 
 
-SELECT brand_id, product_id, list_price FROM product.product ORDER BY brand_id DESC;
 
-SELECT list_price, brand_id, product_id FROM product.product ORDER BY list_price DESC;
+SELECT brand_id, product_id, list_price FROM product.product ORDER BY brand_id, list_price DESC;
+
+SELECT list_price, brand_id, product_id FROM product.product ORDER BY list_price DESC, brand_id ASC;
 
 SELECT TOP(10) * FROM product.product WHERE list_price >=3000;
 
